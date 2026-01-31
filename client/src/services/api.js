fecef5ae663ebd1b65ad9645253bdfd652e441ce
shaffernisha <shaffernisha96@gmail.com>
2026-01-31 12:29:36 +0530

    modified for frontend deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-backend-tqq5.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
